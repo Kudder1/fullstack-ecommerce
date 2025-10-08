88661822c9d271632c6800e2182692ffeff38c7f
Vladimir Fishelev <vladimir.fishelev@dataart.com>
2025-10-08 11:08:57 +0300

    Changes for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-require('dotenv').config({
-  quiet: true
-})
+require('dotenv').config({ quiet: true })
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
@@ -14,12 +12,19 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
-
-app.use(express.json({ limit: '50mb' }));
-
+app.use(express.json({ limit: '50mb' }))
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileupload({}))
 app.use('/api', router)
+
+// --- Serve React frontend ---
+const frontendPath = path.resolve(__dirname, 'public')
+app.use(express.static(frontendPath))
+
+// Fallback route for React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'))
+})
 
 // Error handling, the last middleware
 // As it is the last, we do not call next() inside it
