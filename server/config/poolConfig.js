73e77d645c3afa652dce9f510f22ef918fc75f3a
Vladimir Fishelev <vladimir.fishelev@dataart.com>
2025-11-05 18:06:57 +0200

    update pool, remove logs

--- server/config/poolConfig.js
+++ server/config/poolConfig.js
@@ -1,6 +1,8 @@
 module.exports = {
   max: 25,           // Max connections
   min: 3,            // Min connections always open
-  acquire: 10000,    // 10s timeout to get a connection (fail fast)
-  idle: 10000        // How long before closing idle connection
+  acquire: 30000,    // 30s timeout to get a connection (prevent 499 on high load)
+  idle: 10000,       // How long before closing idle connection
+  evict: 1000,       // Check for idle connections every 1s
+  connectionTimeoutMillis: 5000  // 5s to establish new DB connection
 };
