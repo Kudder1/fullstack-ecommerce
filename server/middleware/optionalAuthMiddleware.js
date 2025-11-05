e7e0beecc89aa999e06693fe2492c17f7696c261
Vladimir Fishelev <vladimir.fishelev@dataart.com>
2025-11-05 12:12:02 +0200

    fix token name

--- server/middleware/optionalAuthMiddleware.js
+++ server/middleware/optionalAuthMiddleware.js
@@ -8,7 +8,7 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization?.split(' ')[1]
         if (token) {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
             req.user = decoded   
         }
         next()
